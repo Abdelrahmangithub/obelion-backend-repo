6a793bcb47fdb4a36553813b127bf4f54f21a1e6
Abdelrahman Ayman <122926460+Abdelrahmangithub@users.noreply.github.com>
2025-11-22 18:49:53 +0200

    Update allowed_origins in CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],        // Any route that starts with api/
     'allowed_methods' => ['*'],       
-    'allowed_origins' => ['http://16.170.173.253:3001'],// Frontend URL ///////////////
+    'allowed_origins' => ['http://51.20.114.27:3001'],// Frontend URL ///////////////
     'allowed_headers' => ['*'],       
     'exposed_headers' => [],
     'max_age' => 0,
