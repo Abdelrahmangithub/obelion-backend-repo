04a783fdeb61f8242cd0a4395836f311ea14db5a
Abdelrahman Ayman <122926460+Abdelrahmangithub@users.noreply.github.com>
2025-11-22 22:20:43 +0200

    Update CORS configuration for local development

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,13 @@
 <?php
 
+// The aim of this configuration file is to set up CORS (Cross-Origin Resource Sharing) settings for a Laravel application.
+// This configuration allows the backend to accept requests from a frontend application running on a different origin (http://localhost:3001).
 return [
-    'paths' => ['api/*'],        // Any route that starts with api/
-    'allowed_methods' => ['*'],       
-    'allowed_origins' => ['http://51.20.114.27:3001'],// Frontend URL ///////////////
-    'allowed_headers' => ['*'],       
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => false,
+    'paths' => ['api/*'],                               // Any route that starts with api/
+    'allowed_methods' => ['*'],                         // Allow all HTTP methods 
+    'allowed_origins' => ['http://localhost:3001'],     // Frontend URL ///////////////
+    'allowed_headers' => ['*'],                         // Allow all headers
+    'exposed_headers' => [],                            // No exposed headers
+    'max_age' => 0,                                     // No caching of preflight response
+    'supports_credentials' => false,                    // Do not support credentials
 ];
